80579a4a55833ef3e2e5df1f43ea513d6d870d88
shahab <shahabrohani178@gmail.com>
2023-01-26 20:32:16 -0800

    testing

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+const div = document.querySelector("div");
+
+function countDown(number) {
+  setInterval(function () {
+    if (number > 0) {
+      number--;
+      div.textContent = number;
+    } else if (number === 0) {
+      clearInterval();
+    }
+  }, 1000);
+}
+
+countDown(10);
+////////////////////////////
+
+let count = 0;
+const numberGen = setInterval(function () {
+  const randomNum = Math.random();
+  if (randomNum >= 0.5) {
+    clearInterval(numberGen);
+    return count;
+  } else {
+    console.log(randomNum);
+    count++;
+  }
+}, 1000);
